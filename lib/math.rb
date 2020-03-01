033a5da1eb8b462eb047678e459cf9ecbcbd4b57
persuasive-vector-4859 <robdefendini@gmail.com>
2020-03-01 20:52:48 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,13 +11,14 @@ num2 = 10
 num2 - num1
 end
 
+def division(num1, num2)
+  num2 / num1
+
+end
+
 def multiplication(num1, num2)
 
 end
-def division(num1, num2)
-
-end
-
 
 def modulo(num1, num2)
 
